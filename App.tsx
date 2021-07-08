d9dcd35044584a0091de77f1889eb875a2ed0221
HJ Sievert <hj.sievert@gmail.com>
2021-07-08 14:21:35 -0700

    Initial code with TS modifications

--- App.tsx
+++ App.tsx
@@ -1,15 +1,63 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
+import { createStackNavigator,  StackNavigationProp } from '@react-navigation/stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  Profile: { userId: string };
+};
+
+type ProfileScreenRouteProp = RouteProp<RootStackParamList, 'Profile'>;
+
+type ProfileScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Profile'
+>;
+
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+  route: ProfileScreenRouteProp;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+const HomeScreen = ({ navigation }: Props) => {
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', { userId: 'Jane' })
+        }
+      />
+      <Text>Ready to Revive Classical Music in style and with navigation!</Text>
+    </View>
+  );
+};
+const ProfileScreen = ({ route, navigation }: Props) => {
+  return <Text>This is {route.params.userId}'s profile</Text>;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <RootStack.Screen
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
