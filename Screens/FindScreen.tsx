a2c470e7774c39ef235c6b947a300a07e3b4ee65
HJ Sievert <hj.sievert@gmail.com>
2021-07-11 22:25:48 -0700

    tuned Find to work on web, main problem was CORS

--- Screens/FindScreen.tsx
+++ Screens/FindScreen.tsx
@@ -34,34 +34,36 @@ const FindScreen = ({ route, navigation }: TabProps) => {
     }, []);
 
     let renderComposer = (item: ComposerType): ReactElement => {
-const bornDate: Date = new Date(item.born)
+        const bornDate: Date = new Date(item.born)
         return (
             <View style={{ flexDirection: 'row', paddingLeft: 10, alignContent: 'center', backgroundColor: 'rgb(183, 211, 255)', borderBottomWidth: 1, borderBottomColor: '#bbb' }}>
-            <Text style={{ flex: 1, fontSize: 14, backgroundColor: 'rgb(183, 211, 255)', textAlign: 'left', paddingTop: 5, paddingBottom: 5, color: 'black' }}>
-              {item.first} <Text style={{fontWeight: 'bold'}}>{item.last}</Text>, born {bornDate.getDay()}. {bornDate.getMonth()} {bornDate.getFullYear()}, {item.period} Period      
-            </Text>
+                <Text style={{ flex: 1, fontSize: 14, backgroundColor: 'rgb(183, 211, 255)', textAlign: 'left', paddingTop: 5, paddingBottom: 5, color: 'black' }}>
+                    {item.first} <Text style={{ fontWeight: 'bold' }}>{item.last}</Text>, born {bornDate.getDate()}. {bornDate.getMonth() + 1} {bornDate.getFullYear()}, {item.period} Period
+                </Text>
             </View>
         )
     };
 
     return (
-        <View style={{ flexDirection: 'column', height: Dimensions.get('window').height - 75 }} >
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 10, paddingTop: 5, paddingBottom: 5, backgroundColor: '#75aaff', borderBottomColor: '#bbb', borderBottomWidth: 1 }}>
-                <Text style={{ fontSize: 18, color: 'black', textAlign: 'left', paddingLeft: 5, paddingTop: 3, paddingBottom: 3 }}>
-                    Find composers
-                </Text>
-            </View>
-            <View style={{ flexShrink: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                <View style={{ paddingBottom: 5, backgroundColor: '#a4c6fc' }}>
-                    <Text style={styles.body}>Are you ready to Revive Classical Music by finding your favorite composer?</Text>
+        <View style={{ height: Dimensions.get('window').height - 75 }} >
+            <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch', paddingBottom: 5, backgroundColor: 'white' }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: 5, paddingLeft: 10, paddingBottom: 5, backgroundColor: '#75aaff', borderBottomColor: '#bbb', borderBottomWidth: 1 }}>
+                    <Text style={{ fontSize: 18, color: 'black', textAlign: 'left', paddingLeft: 5, paddingTop: 3, paddingBottom: 3 }}>
+                        Find composers
+                    </Text>
                 </View>
-            </View>
-            <View style={{ flexShrink: 1, borderTopWidth: 1, borderTopColor: '#666', borderBottomWidth: 1, borderBottomColor: '#666' }}>
-                <FlatList
-                    data={data}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => renderComposer(item)}
-                />
+                <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                    <View style={{ flex: 1, paddingBottom: 50, backgroundColor: '#a4c6fc' }}>
+                        <Text style={styles.body}>Are you ready to Revive Classical Music by finding your favorite composer?</Text>
+                    </View>
+                </View>
+                <View style={{ flexShrink: 1, borderTopWidth: 1, borderTopColor: '#666', borderBottomWidth: 1, borderBottomColor: '#666' }}>
+                    <FlatList
+                        data={data}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => renderComposer(item)}
+                    />
+                </View>
             </View>
         </View>
     );
